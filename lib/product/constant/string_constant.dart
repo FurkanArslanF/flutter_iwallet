f88cee05bb210955c8becec7f668736047362cf4
Furkan Arslan <starzon67@gmail.com>
2024-02-24 13:08:46 +0300

    App String class

---
+++ lib/product/constant/string_constant.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  static const String appName = "iPerson";
+  static const String searchButton = "Kullanıcı Ara";
+  static const String userNotFound = "Kullanıcı bulunamadı!";
+  static const String email = "E-posta";
+  static const String phone = "Telefon";
+  static const String address = "Adres";
+  static const String city = "Şehir";
+  static const String location = "Konum";
+}
